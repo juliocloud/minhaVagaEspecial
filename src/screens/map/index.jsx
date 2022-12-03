8e40a7a530130e7b870779de610424656844e548
juliocjuri <juliocesarjuriolli@gmail.com>
2022-12-03 14:13:43 -0300

    criada tela do mapa

---
+++ src/screens/map/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export function Map(){
+     return(
+          <View>
+               <Text>
+                    Hello World
+               </Text>
+          </View>
+     )
+}
