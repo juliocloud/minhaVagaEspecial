92605029590dbffc40f2fde9f7172487fc35d7ea
juliocjuri <juliocesarjuriolli@gmail.com>
2022-12-03 16:11:56 -0300

    implementado mapa simples

--- src/screens/map/index.jsx
+++ src/screens/map/index.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import MapView from 'react-native-maps';
+
+import { StatusBar } from 'expo-status-bar';
+
+import { styles } from './styles';
 
 export function Map(){
      return(
-          <View>
-               <Text>
-                    Hello World
-               </Text>
+          <View style={styles.container}>
+               <StatusBar hidden={true}/>
+               <MapView 
+                    style={styles.map}
+                    initialRegion={{
+                         latitude: 10,
+                         longitude: 10,
+                         latitudeDelta: 0.00013033,
+                         longitudeDelta: 0.00013033
+                    }}
+                    showsPointsOfInterest={false}
+                    showsUserLocation={true}
+               >
+
+               </MapView>
           </View>
      )
 }
---
+++ src/screens/map/styles.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+     container: {
+          flex: 1,
+          justifyContent: 'flex-end',
+          alignItems: 'flex-end'
+     },
+     map: {
+          width: '100%',
+          height: '100%',
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0
+     }
+})
